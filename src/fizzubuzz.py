96456c83435e6d4fb7f28d70360e6907cdabdc0e
msg.edson <msg.edson@gmail.com>
2026-01-07 21:11:35 -0300

    test: Sample of a dummy test

--- src/fizzubuzz.py
+++ src/fizzubuzz.py
@@ -1,9 +1,11 @@
-def fizzbuzz(n):
+def fizzbuzz(n, additional_rules):
     """
     Convert a number to it`s name in the game FizzBuzz
     """
     answer = ""
     rules = {3: "Fizz", 5: "Buzz"}
+    if additional_rules:
+        rules.update(additional_rules)
     for divisor in sorted(rules.keys()):
         if n % divisor == 0:
             answer += rules[divisor]
--- tests/test_fizzubuzz.py
+++ tests/test_fizzubuzz.py
@@ -12,4 +12,12 @@ from src.fizzubuzz import fizzbuzz
     ],
 )
 def test_fizzbuzz(number, expected):
-    assert fizzbuzz(number) == expected
+    assert fizzbuzz(number, None) == expected
+
+
+@pytest.mark.parametrize(
+    "number, additional_rules, expected",
+    [(7, {7: "Whizz"}, "Whizz")],
+)
+def test_fizzbuzz_with_whizz_too(number, additional_rules, expected):
+    assert fizzbuzz(number, additional_rules) == expected
